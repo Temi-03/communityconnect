6501bff4929b603c9b52805803beef88db060556
Temi-f03 <temiladefetuga@gmail.com>
2025-11-24 12:11:40 +0000

    inital setup

---
+++ app/test.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TestScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "purple", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 40, color: "white" }}>TEST SCREEN WORKS 🎉</Text>
+    </View>
+  );
+}
