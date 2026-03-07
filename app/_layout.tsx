b1f9cf95a1a620b0412cdec36c6974a05db2bbf6
Temi-f03 <temiladefetuga@gmail.com>
2026-03-07 16:05:02 +0000

    feat: implement authentication state handling and loading indicator in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,52 @@
-import { Stack } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { Stack, router, usePathname } from "expo-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="test" />
-    </Stack>
-  );
+  const pathname = usePathname();
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    // listen for login state changes
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        await user.reload();
+
+        const verified = user.emailVerified === true;
+        const onVerifyPage = pathname === "/auth/verifyEmail";
+        const onAuthPages = pathname?.startsWith("/auth");
+
+        if (!verified && !onVerifyPage) {// if logged in but email not verified  force verify screen
+          router.replace("/auth/verifyEmail");
+          return;
+        }
+
+        if (verified && onVerifyPage) {// if user verifies email while on verify page send to home
+          router.replace("/(tabs)/home");
+          return;
+        }
+
+        if (verified && onAuthPages && !onVerifyPage) { // if user is already logged in and tries to open login/signup
+          router.replace("/(tabs)/home");
+          return;
+        }
+      }
+
+      setReady(true);
+    });
+
+    return () => unsubscribe();
+  }, [pathname]);
+
+  if (!ready) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
