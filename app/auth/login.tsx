20d50332e06fd1026ecbea4f859bea9d63e3e943
Temi-f03 <temiladefetuga@gmail.com>
2026-03-10 23:52:51 +0000

    refactor: improve code formatting and readability in login component

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,115 +1,133 @@
 import { useState } from "react";
-import {View,Text,TextInput,Pressable,StyleSheet} from "react-native";
-import { signInWithEmailAndPassword,sendPasswordResetEmail, } from "firebase/auth";
+import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
+import {
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 import { auth } from "../../firebase";
 import { router } from "expo-router";
 
+export default function Login() {
 
-export default function Login() { 
+  //ui state
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+
   async function handleLogin() {
     try {
       setError(""); // Clear previous errors
       setLoading(true); // Set loading state to true to indicate that the login process has started
 
-      if (!email.trim()) throw new Error("Email is required.");
+      if (!email) throw new Error("Email is required.");
       if (!password) throw new Error("Password is required.");
 
+      //
       const userCred = await signInWithEmailAndPassword(
         auth,
         email.trim(),
-        password
+        password,
       );
+      // get the latest user data as firebase sometimes caches info
       await userCred.user.reload();
 
-     if (!userCred.user.emailVerified) {
-      setError("Verify your email first. Tap 'Resend email' if needed.");
-      router.replace("/auth/verifyEmail");
-      return;
-}
+      if (!userCred.user.emailVerified) {
+        setError("Verify your email first. Tap 'Resend email' if needed.");
+        router.replace("/auth/verifyEmail");
+        return;
+      }
       console.log("LOGGED IN:", userCred.user.uid);
       router.replace("/(tabs)/home");
-
-    } 
-  catch (err: any) {
-    if (err?.code === "auth/invalid-email") {
-      setError("Please enter a valid email.");} 
-    else if (err?.code === "auth/invalid-credential") {
-      setError("Incorrect email or password.");} 
-    else {
-      setError(err?.message || "Login failed."); }
-  }
-    finally {
+    } catch (err: any) {
+      if (err?.code === "auth/invalid-email") {
+        setError("Please enter a valid email.");
+      } else if (err?.code === "auth/invalid-credential") {
+        setError("Incorrect email or password.");
+      } else {
+        setError(err?.message || "Login failed.");
+      }
+    } finally {
       setLoading(false); //disables the loading state after the login attempt is finished, regardless of success or failure
     }
   }
 
   async function handleForgotPassword() {
-  try {
-    setError("");
-    const mail = email.trim();
-    if (!mail) throw new Error("Enter your email first.");
-    await sendPasswordResetEmail(auth, mail);
-    setError("Password reset email sent. Check your inbox/spam.");
-  } catch (err: any) {
-    if (err.code === "auth/invalid-email") setError("Please enter a valid email.");
-    else if (err.code === "auth/user-not-found") setError("No account found for that email.");
-    else setError(err.message || "Could not send reset email.");
+    try {
+      setError("");
+      const mail = email.trim();
+      if (!mail) throw new Error("Enter your email first.");
+      await sendPasswordResetEmail(auth, mail);
+      setError("Password reset email sent. Check your inbox/spam.");
+    } catch (err: any) {
+      if (err.code === "auth/invalid-email")
+        setError("Please enter a valid email.");
+      else if (err.code === "auth/user-not-found")
+        setError("No account found for that email.");
+      else setError(err.message || "Could not send reset email.");
+    }
   }
-}
 
   return (
-      <View style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.textContainer}>
-      <Text style={styles.title}>Welcome</Text>
-      <Text style={styles.title}>Back</Text>
-        </View>
-        <View style={styles.formContainer}>
-          {error ? <Text style={styles.errorText}>{error}</Text> : null}
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="#000000"
-            style={styles.input}
-            value={email}
-            onChangeText={setEmail}
-            autoCapitalize="none"
-            keyboardType="email-address"
-          />
-
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="#010101"
-            secureTextEntry
-            style={styles.input}
-            value={password}
-            onChangeText={setPassword}
-          />
-
-          <Pressable
-            style={[styles.loginButton, loading && { opacity: 0.7 }]}
-            onPress={handleLogin}
-            disabled={loading}
-          >
-            <Text style={styles.loginText}>
-              {loading ? "Logging in..." : "Log In"}
-            </Text>
-          </Pressable>
-          <Pressable onPress={handleForgotPassword} disabled={loading} style={{ marginBottom: 12 }}>
-                <Text style={{ color: "white", textDecorationLine: "underline", fontWeight: "600" }}>Forgot password </Text>
-         </Pressable>
-          <Text style={styles.questionText}>Don’t have an account?</Text>
-          <Pressable
-            style={styles.signUpButton}
-            onPress={() => router.push("/auth/signup")}
-          >
-            <Text style={styles.signUpText}>Sign Up</Text>
-          </Pressable>
-        </View>
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.title}>Back</Text>
       </View>
+      <View style={styles.formContainer}>
+        {error ? <Text style={styles.errorText}>{error}</Text> : null}
+        <TextInput
+          placeholder="Email"
+          placeholderTextColor="#000000"
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          keyboardType="email-address"
+        />
 
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="#010101"
+          secureTextEntry
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <Pressable
+          style={[styles.loginButton, loading && { opacity: 0.7 }]}
+          onPress={handleLogin}
+          disabled={loading}
+        >
+          <Text style={styles.loginText}>
+            {loading ? "Logging in..." : "Log In"}
+          </Text>
+        </Pressable>
+        <Pressable
+          onPress={handleForgotPassword}
+          disabled={loading}
+          style={{ marginBottom: 12 }}
+        >
+          <Text
+            style={{
+              color: "white",
+              textDecorationLine: "underline",
+              fontWeight: "600",
+            }}
+          >
+            Forgot password{" "}
+          </Text>
+        </Pressable>
+        <Text style={styles.questionText}>Don’t have an account?</Text>
+        <Pressable
+          style={styles.signUpButton}
+          onPress={() => router.push("/auth/signup")}
+        >
+          <Text style={styles.signUpText}>Sign Up</Text>
+        </Pressable>
+      </View>
+    </View>
   );
 }
 
@@ -191,4 +209,4 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "600",
   },
-}); 
+});
